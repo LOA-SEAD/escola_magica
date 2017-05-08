fede97e7b622e45869ae6205f664a8592c69c2ea
DouglasBarbino <douglas-448@hotmail.com>
2017-05-08 12:50:23 -0300

    Junction of stats.js and statsPlay.js

--- Construct2/Files/stats.js
+++ Construct2/Files/stats.js
@@ -32,3 +32,30 @@ function sendData(correta,pergunta,nroPergunta,respostas,escolhida,acertou,taman
     console.log(acertou);
     console.log(tamanho);*/
 }
+
+function sendPlayData(dano,fase,setor){
+	/*var info = {};
+    var path;
+    if(window.location.hostname == "localhost" ){   // for localhost tests
+        path = "/exported-resource/saveStats" //VAI MUDAR
+    }else {                                 // for web version in production, electron and crosswalk versions
+        path = "http://remar.dc.ufscar.br/exported-resource/saveStats" //VAI MUDAR
+	}
+	$.getJSON("remar.json", function(json) {
+        info.exportedResourceId = json.exportedResourceId;
+        info.damage = dano;
+        info.level = fase;
+        info.sector = setor;
+        info.gameType = 'gameStatsEscolaMagica';
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: info,
+            success: function(data) {
+            }
+        })
+    });*/
+	console.log(dano);
+	console.log(fase);
+	console.log(setor);
+}
