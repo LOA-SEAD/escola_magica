3a72f944a30bfa06f2d95c6e6c13e7f9cd00d327
Luiz Valério Neto <luizvneto@gmail.com>
2019-05-30 09:10:07 -0300

    Plugins
    - Inclusão dos plugins usados para instalação e funcionamento do projeto no Construct2

---
+++ Plugins/joe7_loadJson/edittime.js
@@ -0,0 +1,113 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Load JSON",				
+		"id":			"ljson",
+		"version":		"1.2",			
+		"description":	"Loads and parses a JSON-file.",
+		"author":		"Joe7",
+		"help url":		"http://dev.liebhard.net/",
+		"category":		"Data & Storage",				
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0	
+	};
+};
+
+
+////////////////////////////////////////
+// Conditions
+
+AddCondition(0,	cf_trigger, "On loading completed", "Data", "On loading JSON completed", "Triggered when the loading completes successfully.", "OnLoadingComplete");
+
+////////////////////////////////////////
+// Actions
+AddAction(0, 0, "Load JSON", "Data", "Load JSON", "Load a JSON-file.", "LoadJSON");
+
+////////////////////////////////////////
+// Expressions
+AddStringParam("Keyword", "Enter a keyword for the loading.");
+AddNumberParam("Position", "Enter the position of the data.");
+AddExpression(0, ef_return_string, "Get JSON-data.", "Data", "GetData", "Get JSON-data.");
+
+AddExpression(1, ef_return_string, "Read the loading return.", "Data", "ReadLoadingReturn", "Read the return value.");
+
+AddExpression(2, ef_return_number, "How many rootkeys are in use.", "Data", "GetRootKeysSize", "How many rootkeys are in use.");
+
+AddStringParam("Key", "Enter a key to count it's values.");
+AddExpression(3, ef_return_number, "How many values are in use.", "Data", "GetKeySize", "How many values are in use.");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+
+var property_list = [
+	new cr.Property(ept_text,		'JSON-file',	'languageStrings.json',	'The name and adress of the external JSON-file.'),
+	
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ Plugins/joe7_loadJson/runtime.js
@@ -0,0 +1,190 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+
+cr.plugins_.ljson = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	
+	var pluginProto = cr.plugins_.ljson.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		this.nameOfExternalFile= this.properties[0];
+		this.keysArray= new Array();
+		this.keysContentArray= new Array(); // an array of arrays - key is stored in keysArray
+		
+		this.counterKeys= 0;
+		
+		this.returnValue= "";
+		this.loadingCompleted= false;
+	};
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+
+	cnds.OnLoadingComplete = function ()
+	{
+		return this.loadingCompleted === true;
+	};
+	
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	acts.LoadJSON = function ()
+	{
+		jQuery.ajaxSetup({'beforeSend': function(xhr)
+		{ // if not => browser warnings
+				if (xhr.overrideMimeType) xhr.overrideMimeType("text/plain");
+		}});
+		
+		jQuery.ajaxSetup( { "async": false } ); // instant loading JSON
+		
+		// Create a context object with a reference back to this
+		var context_obj = { inst: this };
+		
+		// Make the request
+		jQuery.ajax(
+		{
+			url: this.nameOfExternalFile,
+			dataType: 'json',
+			context: context_obj,
+			success: function(data)
+			{
+				for (var myJSONkey in data)
+				{
+					var tempArrayWords= new Array();
+					var counterWords= 0;
+					
+					for(var word in data[myJSONkey]) 
+					{
+						tempArrayWords[counterWords]= data[myJSONkey][word];
+						counterWords++;
+					}
+					this.inst.keysArray[this.inst.counterKeys]= myJSONkey; // saving the key
+					this.inst.keysContentArray[this.inst.counterKeys]= tempArrayWords; // saving the values of the key
+					this.inst.counterKeys++;
+				};
+				this.inst.loadingCompleted= true;				
+				this.inst.returnValue= "success";
+				this.inst.runtime.trigger(cr.plugins_.ljson.prototype.cnds.OnLoadingComplete, this.inst);
+			},
+			error: function() 
+			{
+				this.inst.returnValue= "error";
+				this.inst.loadingCompleted= "false";
+			}
+			
+		});
+	};	
+	
+	
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+	
+	exps.GetRootKeysSize = function (ret)	
+	{
+		ret.set_int(this.counterKeys);
+		this.returnValue= "rootkeys size: "+this.counterKeys;
+	};
+	
+	exps.GetKeySize = function (ret, keyWordString)	
+	{
+		for (var i= 0; i< this.keysArray.length; i++)
+		{
+			if (keyWordString == this.keysArray[i]) // search for the keyword
+			{
+				ret.set_int(this.keysContentArray[i].length);
+				this.returnValue= keyWordString+": "+this.keysContentArray[i].length;
+				return;
+			}
+		}
+		// if not found
+		ret.set_int(-1);
+		this.returnValue= "key doesn't exist";
+	};
+	
+	exps.GetData = function (ret, keyWordString, positionInt)	
+	{
+		if (positionInt < 0)
+		{
+			ret.set_string("Not a positive number!");
+			this.returnValue= "negative number";
+			return;
+		}
+		
+		try
+		{
+			for (var i= 0; i< this.keysArray.length; i++)
+			{
+				if ((keyWordString == this.keysArray[i]) && (positionInt < this.keysContentArray[i].length))
+				{
+					ret.set_string(this.keysContentArray[i][positionInt]);
+					this.returnValue= "success";
+					return;
+				}
+			}
+			this.returnValue= "nothing";
+			ret.set_string("nothing found");
+		}
+		catch (err)
+		{
+			ret.set_string(err);
+			this.returnValue= err;
+		}
+		
+	};
+	
+	exps.ReadLoadingReturn = function (ret)	
+	{
+		ret.set_string(this.returnValue);
+	};
+
+}());
